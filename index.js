5c330f651f82634fc0546ba6ea413693ade6f708
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-07-09 18:41:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 
+const prizes = ['A Unicorn!', 'A Hug!', 'Fresh Laundry!']
+ for (var btnNum = 0; btnNum < prizes.length; btnNum++) {
+   //For each of our buttons when the user clicks it... 
+   document.getElementById(`btn-${btnNum}`).onclick = () => {
+     //Tell her what she's won!
+     document.getElementById("prize").innerHTML = prizes[btnNum];
+   }
+ }
